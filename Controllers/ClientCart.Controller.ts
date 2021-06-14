997197b46fe3cd9bd103a6f34476af116a9795e9
lambiengcode <lambiengcode@gmail.com>
2021-06-14 18:44:41 +0700

    [edit] edit update cart client controller

--- Controllers/ClientCart.Controller.ts
+++ Controllers/ClientCart.Controller.ts
@@ -1,263 +1,275 @@
-import { CartPath } from '../common/RoutePath';
-import { Response, NextFunction } from 'express';
-import Controller, { Methods } from './Controller';
-import TokenServices from '../Services/Token.Services';
-import { IValidateRequest } from '../common/DefineRequest';
-import Validate from '../Validates/Validate';
-import RoleInstance from '../common/RoleInstance';
-import ClientCartServices from '../Services/ClientCart.Services';
-import schemaClientCart from '../Validates/ClientCart.Validate'
-import UserService from '../Services/User.Services';
-import PackageService from '../Services/Package.Services';
+import { CartPath } from "../common/RoutePath";
+import { Response, NextFunction } from "express";
+import Controller, { Methods } from "./Controller";
+import TokenServices from "../Services/Token.Services";
+import { IValidateRequest } from "../common/DefineRequest";
+import Validate from "../Validates/Validate";
+import RoleInstance from "../common/RoleInstance";
+import ClientCartServices from "../Services/ClientCart.Services";
+import schemaClientCart from "../Validates/ClientCart.Validate";
+import UserService from "../Services/User.Services";
+import PackageService from "../Services/Package.Services";
 export default class ClientCartController extends Controller {
-	path = '/User/Client';
-	routes = [
-		{
-			path: `/${CartPath.ADD_PRODUCT_TO_CART}`,
-			method: Methods.POST,
-			handler: this.handleAddProductToCart,
-			localMiddleware: [
-				TokenServices.verify,
-				RoleInstance.getInstance().isRole([]),
-				Validate.body(schemaClientCart.addProductToCartid)
-			]
-		},
-		{
-			path: `/${CartPath.DELETE_PRODUCT_FROM_CART}`,
-			method: Methods.POST,
-			handler: this.handleDeleteProductFromCart,
-			localMiddleware: [
-				TokenServices.verify,
-				RoleInstance.getInstance().isRole([]),
-				Validate.body(schemaClientCart.deleteProductFromCart)
-			]
-		},
-		{
-			path: `/${CartPath.GET_MY_CART}`,
-			method: Methods.GET,
-			handler: this.handleGetMyCart,
-			localMiddleware: [
-				TokenServices.verify,
-				RoleInstance.getInstance().isRole([]),
-				
-			]
-		},
-		{
-			path: `/${CartPath.PAYMENT_CART}`,
-			method: Methods.POST,
-			handler: this.handlePaymentCart,
-			localMiddleware: [
-				TokenServices.verify,
-				RoleInstance.getInstance().isRole([]),
-				Validate.body(schemaClientCart.paymentPointClientCart)
-			]
-		},
-		{
-			path: `/${CartPath.UPDATE_PRODUCT_FROM_CART}`,
-			method: Methods.POST,
-			handler: this.handleUpdateProductFromCart,
-			localMiddleware: [
-				TokenServices.verify,
-				RoleInstance.getInstance().isRole([]),
-				Validate.body(schemaClientCart.updateProductToCartid)
-			]
-		}
-	];
-	constructor() {
-		super();
-	}
+  path = "/User/Client";
+  routes = [
+    {
+      path: `/${CartPath.ADD_PRODUCT_TO_CART}`,
+      method: Methods.POST,
+      handler: this.handleAddProductToCart,
+      localMiddleware: [
+        TokenServices.verify,
+        RoleInstance.getInstance().isRole([]),
+        Validate.body(schemaClientCart.addProductToCartid),
+      ],
+    },
+    {
+      path: `/${CartPath.DELETE_PRODUCT_FROM_CART}`,
+      method: Methods.POST,
+      handler: this.handleDeleteProductFromCart,
+      localMiddleware: [
+        TokenServices.verify,
+        RoleInstance.getInstance().isRole([]),
+        Validate.body(schemaClientCart.deleteProductFromCart),
+      ],
+    },
+    {
+      path: `/${CartPath.GET_MY_CART}`,
+      method: Methods.GET,
+      handler: this.handleGetMyCart,
+      localMiddleware: [
+        TokenServices.verify,
+        RoleInstance.getInstance().isRole([]),
+      ],
+    },
+    {
+      path: `/${CartPath.PAYMENT_CART}`,
+      method: Methods.POST,
+      handler: this.handlePaymentCart,
+      localMiddleware: [
+        TokenServices.verify,
+        RoleInstance.getInstance().isRole([]),
+        Validate.body(schemaClientCart.paymentPointClientCart),
+      ],
+    },
+    {
+      path: `/${CartPath.UPDATE_PRODUCT_FROM_CART}`,
+      method: Methods.POST,
+      handler: this.handleUpdateProductFromCart,
+      localMiddleware: [
+        TokenServices.verify,
+        RoleInstance.getInstance().isRole([]),
+        Validate.body(schemaClientCart.updateProductToCartid),
+      ],
+    },
+  ];
+  constructor() {
+    super();
+  }
 
-	async handleAddProductToCart(
-		req: IValidateRequest | any,
-		res: Response,
-		next: NextFunction
-	): Promise<void> {
-		try {
-			const idUser = req.value.body.token.data;
-			const objData: any = {
-				name: req.value.body.name,
-    		weight:req.value.body.weight,
-    		type: req.value.body.type,
-    		image: req.value.body.image.split(" ")
-			};
-			const clientCartServices: ClientCartServices = new ClientCartServices();
-			const result = await clientCartServices.addProductToCart(idUser, objData);
-			if (result.success) {
-				super.sendSuccess(res, result.data, result.message);
-			} else {
-				super.sendError(res, result.message);
-			}
-		} catch {
-			super.sendError(res);
-		}
-	}
+  async handleAddProductToCart(
+    req: IValidateRequest | any,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    try {
+      const idUser = req.value.body.token.data;
+      const objData: any = {
+        name: req.value.body.name,
+        weight: req.value.body.weight,
+        type: req.value.body.type,
+        image: req.value.body.image.split(" "),
+      };
+      const clientCartServices: ClientCartServices = new ClientCartServices();
+      const result = await clientCartServices.addProductToCart(idUser, objData);
+      if (result.success) {
+        super.sendSuccess(res, result.data, result.message);
+      } else {
+        super.sendError(res, result.message);
+      }
+    } catch {
+      super.sendError(res);
+    }
+  }
 
-	async handleDeleteProductFromCart(
-		req: IValidateRequest | any,
-		res: Response,
-		next: NextFunction
-	): Promise<void> {
-		try {
-			const idUser: string = req.value.body.token.data;
-			const idProduct: string = req.value.body.idProduct;
+  async handleDeleteProductFromCart(
+    req: IValidateRequest | any,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    try {
+      const idUser: string = req.value.body.token.data;
+      const idProduct: string = req.value.body.idProduct;
 
-			const clientCartServices: ClientCartServices =
-				new ClientCartServices();
-			const result = await clientCartServices.deleteProductFromCart(
-				idUser,
-				idProduct
-			);
-			if (result.success) {
-				super.sendSuccess(res, result.data, result.message);
-			} else {
-				super.sendError(res, result.message);
-			}
-		} catch {
-			super.sendError(res);
-		}
-	}
+      const clientCartServices: ClientCartServices = new ClientCartServices();
+      const result = await clientCartServices.deleteProductFromCart(
+        idUser,
+        idProduct
+      );
+      if (result.success) {
+        super.sendSuccess(res, result.data, result.message);
+      } else {
+        super.sendError(res, result.message);
+      }
+    } catch {
+      super.sendError(res);
+    }
+  }
 
-	async handleGetMyCart(
-		req: IValidateRequest | any,
-		res: Response,
-		next: NextFunction
-	): Promise<void> {
-		try {
-			const idUser = req.value.body.token.data;
-			const clientCartServices: ClientCartServices = new ClientCartServices();
-			const result = await clientCartServices.getMyCart(idUser);
-			if (result.success) {
-				super.sendSuccess(res, result.data, result.message);
-			} else {
-				super.sendError(res, result.message);
-			}
-		} catch {
-			super.sendError(res);
-		}
-	}
-	async handlePaymentCart(
-		req: IValidateRequest | any,
-		res: Response,
-		next: NextFunction
-	): Promise<void> {
-		try {
-			const idUser: string = req.value.body.token.data;
+  async handleGetMyCart(
+    req: IValidateRequest | any,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    try {
+      const idUser = req.value.body.token.data;
+      const clientCartServices: ClientCartServices = new ClientCartServices();
+      const result = await clientCartServices.getMyCart(idUser);
+      if (result.success) {
+        super.sendSuccess(res, result.data, result.message);
+      } else {
+        super.sendError(res, result.message);
+      }
+    } catch {
+      super.sendError(res);
+    }
+  }
+  async handlePaymentCart(
+    req: IValidateRequest | any,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    try {
+      const idUser: string = req.value.body.token.data;
 
-			const {
-				title,
-				description,
-				estimatedDate,
-				
-				FK_Transport,
-				FK_SubTransport,
-				FK_SubTransportAwait,
-				
-				recipientName,
-				recipientAddress,
-				recipientLat,
-				recipientLng,
-				recipientPhone,
-				prices,
-				distance,
-				weight,
+      const {
+        title,
+        description,
+        estimatedDate,
 
-				senderPhone,
-				senderAddress,
-				senderLat,
-				senderLng
-			} = req.value.body;
+        FK_Transport,
+        FK_SubTransport,
+        FK_SubTransportAwait,
 
-			//get user
-			const userService: UserService = new UserService();
-			const user = await userService.getInfo(idUser);
-      console.log(`LHA:  ===> file: MerchantCart.Controller.ts ===> line 167 ===> user`, user)
-			const converPriceToPoint=~~+prices/5000
-			if(user.data.point>converPriceToPoint)
-			{
-				user.data.point=user.data.point-10
-				await user.data.save()
-	
-				const clientCartServices:ClientCartServices=new ClientCartServices();
-				const resClientCart=await clientCartServices.paymentCart(idUser)
-	
-				const obj: any = {
-					title,
-					description,
-					estimatedDate,
-					FK_Recipient: idUser,
-					FK_Transport,
-					FK_SubTransport,
-					FK_SubTransportAwait,
-					prices,
-					distance,
-					weight,
-					FK_Product: resClientCart.data.products, //Get from cart
-					FK_ProductType: 'Standard', //Get from cart
-					recipient: {
-						name: recipientName,
-						location: {
-							address: recipientAddress,
-							coordinate: {
-								lat: recipientLat,
-								lng: recipientLng
-							}
-						},
-						phone: recipientPhone
-					},
-					sender: {
-						name: user.data.fullName,
-						location: {
-							address: senderAddress,
-							coordinate: {
-								lat: senderLat,
-								lng:senderLng
-							}
-						},
-						phone: senderPhone
-					},
-					
-				};
-				console.log(`LHA:  ===> file: ClientCart.Controller.ts ===> line 183 ===> obj`, obj)
-				super.sendSuccess(res, obj,"Your points are not enough to pay for this order");
-				return
-				console.log(`LHA:  ===> file: MerchantCart.Controller.ts ===> line 192 ===> obj`, obj)
-				const packageService: PackageService = new PackageService();
-				const result=await packageService.createPackage(obj,false)
-				if (result.success) {
-					super.sendSuccess(res,{}, result.message);
-					return;
-				} else {
-					super.sendError(res, result.message);
-				}
-			}
-			super.sendError(res, "Your points are not enough to pay for this order");
-		} catch {
-			super.sendError(res);
-		}
-	}
-	async handleUpdateProductFromCart(
-		req: IValidateRequest | any,
-		res: Response,
-		next: NextFunction
-	): Promise<void> {
-		try {
-			const idUser = req.value.body.token.data;
-			const objData: any = {
-				idProduct:req.value.body.idProduct,
-				name: req.value.body.name,
-    		weight:req.value.body.weight,
-    		type: req.value.body.type,
-    		image: req.value.body.image
-			};
-			const clientCartServices: ClientCartServices = new ClientCartServices();
-			const result = await clientCartServices.updateProductFromCart(idUser, objData);
-			if (result.success) {
-				super.sendSuccess(res, result.data, result.message);
-			} else {
-				super.sendError(res, result.message);
-			}
-		} catch {
-			super.sendError(res);
-		}
-	}
+        recipientName,
+        recipientAddress,
+        recipientLat,
+        recipientLng,
+        recipientPhone,
+        prices,
+        distance,
+        weight,
+
+        senderPhone,
+        senderAddress,
+        senderLat,
+        senderLng,
+      } = req.value.body;
+
+      //get user
+      const userService: UserService = new UserService();
+      const user = await userService.getInfo(idUser);
+      console.log(
+        `LHA:  ===> file: MerchantCart.Controller.ts ===> line 167 ===> user`,
+        user
+      );
+      const converPriceToPoint = ~~+prices / 5000;
+      if (user.data.point > converPriceToPoint) {
+        user.data.point = user.data.point - 10;
+        await user.data.save();
+
+        const clientCartServices: ClientCartServices = new ClientCartServices();
+        const resClientCart = await clientCartServices.paymentCart(idUser);
+
+        const obj: any = {
+          title,
+          description,
+          estimatedDate,
+          FK_Recipient: idUser,
+          FK_Transport,
+          FK_SubTransport,
+          FK_SubTransportAwait,
+          prices,
+          distance,
+          weight,
+          FK_Product: resClientCart.data.products, //Get from cart
+          FK_ProductType: "Standard", //Get from cart
+          recipient: {
+            name: recipientName,
+            location: {
+              address: recipientAddress,
+              coordinate: {
+                lat: recipientLat,
+                lng: recipientLng,
+              },
+            },
+            phone: recipientPhone,
+          },
+          sender: {
+            name: user.data.fullName,
+            location: {
+              address: senderAddress,
+              coordinate: {
+                lat: senderLat,
+                lng: senderLng,
+              },
+            },
+            phone: senderPhone,
+          },
+        };
+        console.log(
+          `LHA:  ===> file: ClientCart.Controller.ts ===> line 183 ===> obj`,
+          obj
+        );
+        super.sendSuccess(
+          res,
+          obj,
+          "Your points are not enough to pay for this order"
+        );
+        return;
+        console.log(
+          `LHA:  ===> file: MerchantCart.Controller.ts ===> line 192 ===> obj`,
+          obj
+        );
+        const packageService: PackageService = new PackageService();
+        const result = await packageService.createPackage(obj, false);
+        if (result.success) {
+          super.sendSuccess(res, {}, result.message);
+          return;
+        } else {
+          super.sendError(res, result.message);
+        }
+      }
+      super.sendError(res, "Your points are not enough to pay for this order");
+    } catch {
+      super.sendError(res);
+    }
+  }
+  async handleUpdateProductFromCart(
+    req: IValidateRequest | any,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    try {
+      const idUser = req.value.body.token.data;
+      const objData: any = {
+        idProduct: req.value.body.idProduct,
+        name: req.value.body.name,
+        weight: req.value.body.weight,
+        type: req.value.body.type,
+        image: req.value.body.image.split(" "),
+      };
+      const clientCartServices: ClientCartServices = new ClientCartServices();
+      const result = await clientCartServices.updateProductFromCart(
+        idUser,
+        objData
+      );
+      if (result.success) {
+        super.sendSuccess(res, result.data, result.message);
+      } else {
+        super.sendError(res, result.message);
+      }
+    } catch {
+      super.sendError(res);
+    }
+  }
 }
--- Validates/ClientCart.Validate.ts
+++ Validates/ClientCart.Validate.ts
@@ -5,46 +5,46 @@ const validate = {
     name: joi.string().required(),
     weight: joi.string().required(),
     type: joi.string().required(),
-    image: joi.string().required()
-	}),
-	updateProductToCartid: joi.object().keys({
-		idProduct:joi.string().required(),
-		name: joi.string().required(),
+    image: joi.string().required(),
+  }),
+  updateProductToCartid: joi.object().keys({
+    idProduct: joi.string().required(),
+    name: joi.string().required(),
     weight: joi.string().required(),
     type: joi.string().required(),
-    image: joi.array().items(joi.string())
-	}),
+    image: joi.string().required(),
+  }),
   deleteProductFromCart: joi.object().keys({
     idProduct: joi.string().required(),
   }),
-  paymentPointClientCart:joi.object().keys({
+  paymentPointClientCart: joi.object().keys({
     title: joi.string().required(),
-		description: joi.string().min(0).max(100).required(),
+    description: joi.string().min(0).max(100).required(),
 
-		recipientAddress: joi.string().empty().required(),
-		recipientLat: joi.string().empty().required(),
-		recipientLng: joi.string().empty().required(),
-		recipientPhone: joi
-			.string()
-			.required()
-			.regex(/^0\d{9,14}$/),
+    recipientAddress: joi.string().empty().required(),
+    recipientLat: joi.string().empty().required(),
+    recipientLng: joi.string().empty().required(),
+    recipientPhone: joi
+      .string()
+      .required()
+      .regex(/^0\d{9,14}$/),
 
     recipientName: joi.string().empty().required(),
-		senderPhone: joi
-			.string()
-			.required()
-			.regex(/^0\d{9,14}$/),
-		senderAddress: joi.string().empty().required(),
-		senderLat: joi.string().empty().required(),
-		senderLng: joi.string().empty().required(),
+    senderPhone: joi
+      .string()
+      .required()
+      .regex(/^0\d{9,14}$/),
+    senderAddress: joi.string().empty().required(),
+    senderLat: joi.string().empty().required(),
+    senderLng: joi.string().empty().required(),
 
-		estimatedDate: joi.string().empty().required(),
-		FK_Transport: joi.string().empty().required(),
-		FK_SubTransport: joi.string().empty().required(),
-		FK_SubTransportAwait: joi.string().empty().required(),
-		distance: joi.string().empty().required(),
-		prices: joi.string().empty().required(),
-		weight: joi.string().empty().required(),
-  })
-}
+    estimatedDate: joi.string().empty().required(),
+    FK_Transport: joi.string().empty().required(),
+    FK_SubTransport: joi.string().empty().required(),
+    FK_SubTransportAwait: joi.string().empty().required(),
+    distance: joi.string().empty().required(),
+    prices: joi.string().empty().required(),
+    weight: joi.string().empty().required(),
+  }),
+};
 export default validate;
